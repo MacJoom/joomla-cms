05ec137f0f63fa5b9353a3dd67a9a5d84855da57
Martin Kopp <kopp05@infotech.ch>
2024-04-29 16:00:37 +0200

    revert Version

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -55,7 +55,7 @@ final class Version
      * @var    integer
      * @since  3.8.0
      */
-    public const PATCH_VERSION = 3;
+    public const PATCH_VERSION = 4;
 
     /**
      * Extra release version info.
