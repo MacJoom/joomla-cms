a54cc36de9e7bf4ecef2a5400a04fb30e505c5f3
Martin Kopp <kopp05@infotech.ch>
2024-04-29 16:08:05 +0200

    restore pkg_search, fix Version.php

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -55,7 +55,7 @@ final class Version
      * @var    integer
      * @since  3.8.0
      */
-    public const PATCH_VERSION = 4;
+    public const PATCH_VERSION = 5;
 
     /**
      * Extra release version info.
